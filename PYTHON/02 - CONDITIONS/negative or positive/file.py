17c6c0e20ae751fb08b88e98663c258907b46d18
Cybug Timehin <93569544+Cybugg@users.noreply.github.com>
2022-05-30 00:00:27 +0100

    update

---
+++ PYTHON/02 - CONDITIONS/negative or positive/file.py
@@ -0,0 +1,15 @@
+# Collect input 
+a = input("Enter num:")
+
+# convert input to float
+a = float(a)
+
+# If number is greater than zero
+if a > 0:
+    print("Number is positive")
+elif a < 0:
+    # If number is lesser than zero
+    print("Number is Negative")
+else:  
+    # all other cases  
+    print("zero")
