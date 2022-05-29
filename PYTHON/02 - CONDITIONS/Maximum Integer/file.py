9a97e5059f24a815bcfdbf5837434ae09a839650
Cybug Timehin <93569544+Cybugg@users.noreply.github.com>
2022-05-30 00:16:46 +0100

    Update file.py

--- PYTHON/02 - CONDITIONS/Maximum Integer/file.py
+++ PYTHON/02 - CONDITIONS/Maximum Integer/file.py
@@ -5,3 +5,9 @@ c = input("enter num: ")
 
 # if a is greater than b and greater than c,
 # then it is the maximum
+if b <= a >= c:
+    print(a,"is the Maximum")
+# Otherwise, we check the number B,
+# that is greater than both A and C.
+elif a <= b >= c:
+    print(b,"is tha maximum")
