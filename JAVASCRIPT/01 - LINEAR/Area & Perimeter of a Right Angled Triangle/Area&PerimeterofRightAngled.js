75499269f26f2d23420ea212a7e649e1f0136cba
Adebiyi Itunuayo <44436048+FFFF-0000h@users.noreply.github.com>
2022-08-05 19:04:25 +0100

    Update Area&Perimeter of a Right Angled Triangle

--- JAVASCRIPT/01 - LINEAR/Area & Perimeter of a Right Angled Triangle/Area&PerimeterofRightAngled.js
+++ JAVASCRIPT/01 - LINEAR/Area & Perimeter of a Right Angled Triangle/Area&PerimeterofRightAngled.js
@@ -1,23 +1,20 @@
-//  Here is a program to calculate the perimeter 
-//  and area of a right-angled triangle 
-//  using two legs entered as input
+/*  Here is a program to calculate the perimeter
+ *  and area of a right-angled triangle
+*/  using two legs entered as input
 
 
 
 //  prompt() returns a string
-let  AB = prompt("Length of the first leg: ")
-let AC = prompt("Length of the second leg: ")
+//  parseFloat() returns real number if it can be converted 
+let  AB = parseFloat(prompt("Length of the first leg: "));
+let AC = parseFloat(prompt("Length of the second leg: "));
 
-//  parseFloat() returns real number if it can be converted
-AB = parseFloat(AB)
-AC = parseFloat(AC)
-
-//  find the hypotenus 
-//  by the pythagorean theorem:
-//  "the sum of the squares of the legs
-//  is equal to the sqaure of the hypotenuse."
-//  The Math.Sqrt() function of the math module
-//  returns a squareroot.
+/*  find the hypotenus
+ *  by the pythagorean theorem:
+ *  "the sum of the squares of the legs
+ *  is equal to the sqaure of the hypotenuse."
+ *  The Math.Sqrt() function of the math module
+*/  returns a squareroot.
 
 const BC = Math.sqrt(AB**2 + AC**2)
 
@@ -29,5 +26,4 @@ const area = (AB*AC) / 2
 const perimeter = AB + AC + BC
 
 
-console.log(`area of the triangle: ${area}`)
-console.log(`perimeter of the triangle: ${perimeter}`)
+console.log(`area of the triangle: ${area}\nperimeter of the triangle: ${perimeter}`)
