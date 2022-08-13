f720b9a30ab2d73b2d45a071e2e164262fd1df78
Adebiyi Itunuayo <44436048+FFFF-0000h@users.noreply.github.com>
2022-08-13 04:24:49 +0100

    Update letterNumberInAlphabet.js
    Typo corrected

--- JAVASCRIPT/01 - LINEAR/Letter Number in an Alphabet/letterNumberInAlphabet.js
+++ JAVASCRIPT/01 - LINEAR/Letter Number in an Alphabet/letterNumberInAlphabet.js
@@ -6,7 +6,7 @@ const code = c.charCodeAt(0)
 
 // code at "a" = 97 
 // being the first letter in the small letter alphabet
-// therfore the distance can be gotten by subtracting 
+// therefore the distance can be gotten by subtracting 
 // the code of the first letter from the code of the input letter
 // and then the position can be gotten by adding 1
 position_ = code - 97 + 1
